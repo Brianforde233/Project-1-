de9aab9d117e32c89b0a4bf2d2ca9b6ac5f78036
Brian Forde <67812860+Brianforde233@users.noreply.github.com>
2020-08-27 22:37:29 -0500

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/**.
+*printng out Java Letters 
+*
+*Project 1 
+*@author Brian Forde-Comp121001
+*@version 8/27/2020
+*/
+public class JLetter
+{
+  /**.
+  *Java 
+  *@param args Command line arguments-not used.
+  */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("J     JAVA  ");
+      System.out.println("JA    JAVA  ");
+      System.out.println(" JAVAJAVA   ");
+      System.out.println("  JAVAJA    ");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,36 @@
+/**.
+* my life goals
+* 
+* Project 1 
+*@author Brian Forde-Comp1210-001
+*@version 8/27/20
+*/
+public class MyLifeGoals 
+{
+    /**.
+    * goals 
+    *@param args Command line arguments-not used. 
+    */
+   public static void main(String[] args)
+   {
+      
+      System.out.println("Brian Forde");
+      System.out.println();
+      System.out.println("My short term life goals " 
+                + "is to finish this semester off"                    
+                + ", continuing to work through hard assignments "
+                + "and staying on top of my work.");
+      System.out.println("My medium life term goals"
+                + " is to finish college on track and"
+                + " finish army rotc to be "
+                + " commissioned in the" 
+                + " army as a second lieutenant."); 
+      System.out.println("My long term life goals "
+                + " is to have a" 
+                + " successful career that supports"
+                + " my family and" 
+                + " have the opportunity to work for" 
+                + " a awesome company such"
+                + " as NASA or SpaceX.");
+   }
+}
